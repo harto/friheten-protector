42f32de419b7c37eb0e50359de5a303f34347a0f
Stuart Campbell <stuart@harto.org>
2015-08-02 20:22:00 -0700

    Fix all the things, use new config

--- sauron/__init__.py
+++ sauron/__init__.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python
-
 import numpy
 import cv2
 import os
-#import sys
+import datetime
 import time
-
-# fixme: better names
-BLUR_KERNEL = (21, 21)
-FRAME_WIDTH = 500
-MIN_CHANGE_AREA = 500
-MIN_CHANGE_THRESHOLD = 50
-OUTPUT_CODEC = cv2.cv.CV_FOURCC(*'MPEG') #cv2.VideoWriter_fourcc(*'MPEG')
+import sauron.config
+from sauron.config import get as get_config
 
 def resize(image, width):
     orig_h, orig_w = image.shape[:2]
@@ -23,15 +16,15 @@ def preprocess_frame(frame):
     # Colour is irrelevant to motion detection
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Remove noise etc.
-    frame = cv2.GaussianBlur(frame, BLUR_KERNEL, 0)
+    frame = cv2.GaussianBlur(frame, get_config('BLUR_KERNEL'), 0)
     return frame
 
 def obscured_regions(image):
     (_, contours, _) = cv2.findContours(image.copy(),
-                                        cv2.RETR_EXTERNAL,
-                                        cv2.CHAIN_APPROX_SIMPLE)
+                                       cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_SIMPLE)
     return (cv2.boundingRect(c) for c in contours
-            if cv2.contourArea(c) >= MIN_CHANGE_AREA)
+            if cv2.contourArea(c) >= get_config('MIN_CHANGE_AREA'))
 
 BACKGROUND = None
 
@@ -41,14 +34,17 @@ def process_frame(raw):
     if BACKGROUND is None:
         BACKGROUND = frame
     frame = cv2.absdiff(BACKGROUND, frame)
-    _, frame = cv2.threshold(frame, MIN_CHANGE_THRESHOLD, 255, cv2.THRESH_BINARY)
+    _, frame = cv2.threshold(frame,
+                             get_config('MIN_CHANGE_THRESHOLD'),
+                             255,
+                             cv2.THRESH_BINARY)
     # dilate to join broken contours
     frame = cv2.dilate(frame, None, iterations=2)
     rects = obscured_regions(frame)
     for (x, y, w, h) in rects:
         cv2.rectangle(raw, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
-    # cv2.imshow('raw', raw)
-    # cv2.imshow('filtered', frame)
+    cv2.imshow('raw', raw)
+    cv2.imshow('filtered', frame)
 
 def key_pressed(key):
     return cv2.waitKey(1) & 0xFF == ord(key)
@@ -67,16 +63,22 @@ def finished():
 def process_video(input, output):
     for frame in frames(input):
         if finished(): break
-        resized = resize(frame, width=FRAME_WIDTH)
+        resized = resize(frame, width=get_config('INPUT_FRAME_WIDTH'))
         process_frame(resized)
-        output.write(resized)
+        # output.write(resized)
 
 if __name__ == '__main__':
+    sauron.config.init()
     cam = cv2.VideoCapture(0)
-    path = os.path.join(os.path.dirname(__file__), 'tmp', 'out.mpg')
+    path = os.path.join(os.path.dirname(__file__), 'tmp', datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S.mpg'))
     print 'writing to %s' % path
-    file = cv2.VideoWriter(path, OUTPUT_CODEC, 24.0, (500, 375), True)
-    time.sleep(1) # fixme: proper wait
+    output_codec = cv2.VideoWriter_fourcc(*get_config('OUTPUT_FORMAT')) #cv2.VideoWriter_fourcc(*'MPEG')
+    file = cv2.VideoWriter(path,
+                           output_codec,
+                           get_config('OUTPUT_FPS'),
+                           get_config('OUTPUT_FRAME_SIZE'),
+                           True)
+    time.sleep(2) # fixme: proper wait
     process_video(cam, file)
     cam.release()
     file.release()
--- sauron/config.py
+++ sauron/config.py
@@ -2,15 +2,23 @@ from derpconf.config import Config
 from os import path
 
 Config.define('INPUT_FRAME_WIDTH', 500, 'Downscaled width of input frames', 'Input')
-Config.define('BLUR_KERNEL_SIZE', 20, 'Size of blur filter for input normalisation', 'Input')
+Config.define('BLUR_KERNEL', (21, 21), 'Size of blur filter for input normalisation (must be odd numbers)', 'Input')
+
 Config.define('MIN_CHANGE_THRESHOLD', 50, 'Pixel value change threshold for detection events', 'Detection')
 Config.define('MIN_CHANGE_AREA', 500, 'Minimum number of changed pixels for detection events', 'Detection')
-Config.define('OUTPUT_FRAME_WIDTH', 500, 'Width of output frames', 'Output')
+
+Config.define('OUTPUT_FRAME_SIZE', (500, 375), 'Width of output frames', 'Output')
 Config.define('OUTPUT_FPS', 24, 'Frames per second of output video', 'Output')
 Config.define('OUTPUT_FORMAT', 'MPEG', 'Four character code of output video format', 'Output')
 
-def load_config(path):
-    conf = Config.load(path)
+_config = None
 
-# root_dir = path.join(path.dirname(__file__), '..')
-# config_path = path.join(root_dir, 'sauron.conf')
+def get(key):
+    return _config.get(key)
+
+def init(path=None):
+    global _config
+    if path is None:
+        _config = Config()
+    else:
+        _config = Config.load(path)
