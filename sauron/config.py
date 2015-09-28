624f0539fed2f3d925ba264b5e0ec1c7585634bd
Stuart Campbell <stuart@harto.org>
2015-09-28 09:53:16 -0700

    Remove unreferenced config

--- sauron/config.py
+++ sauron/config.py
@@ -10,7 +10,6 @@ Config.define('MIN_CHANGE_AREA', 400, 'Minimum number of changed pixels for dete
 
 Config.define('OUTPUT_DIR', path.join(path.dirname(__file__), '..', 'tmp'), 'Directory to store captures in', 'Output')
 Config.define('OUTPUT_FPS', 24, 'Frames per second of output video', 'Output')
-Config.define('OUTPUT_FORMAT', 'MJPG', 'Four character code of output video format', 'Output')
 
 _config = None
 
